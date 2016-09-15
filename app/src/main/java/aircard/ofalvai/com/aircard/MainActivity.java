c5a869cd7b00ba7cc5c9d5c60a212ac3d78fa08f
Olivér Falvai <ofalvai@gmail.com>
2016-09-15 08:21:08 +0200

    Initial commit

---
+++ app/src/main/java/aircard/ofalvai/com/aircard/MainActivity.java
@@ -0,0 +1,13 @@
+package aircard.ofalvai.com.aircard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
