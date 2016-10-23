8a295193949cd7cc65b13c93d5e59a35e8572ea2
Olivér Falvai <ofalvai@gmail.com>
2016-10-23 21:21:10 +0200

    "My cards" DB schema

--- app/src/main/java/com/ofalvai/aircard/db/DbSchema.java
+++ app/src/main/java/com/ofalvai/aircard/db/DbSchema.java
@@ -28,6 +28,42 @@ public class DbSchema {
 
             public static final String TIMESTAMP_SAVE = "timestamp_save";
         }
+
+        public static final String CREATE_QUERY = String.format("CREATE TABLE %1$s (%2$s, %3$s, %4$s, %5$s, %6$s, %7$s, %8$s, %9$s, %10$s DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, %11$s)",
+                TABLE_NAME, Cols.NAME, Cols.MAIL, Cols.PHONE, Cols.ADDRESS, Cols.URL, Cols.NOTE,
+                Cols.CARD_STYLE, Cols.COLOR, Cols.TIMESTAMP_SAVE, Cols.UUID);
+    }
+
+    public static final class MyCardsTable {
+
+        public static final String TABLE_NAME = "my_cards";
+
+        public static final class Cols {
+
+            public static final String UUID = "uuid";
+
+            public static final String NAME = "name";
+
+            public static final String MAIL = "mail";
+
+            public static final String PHONE = "phone";
+
+            public static final String ADDRESS = "address";
+
+            public static final String URL = "url";
+
+            public static final String NOTE = "note";
+
+            public static final String CARD_STYLE= "card_style";
+
+            public static final String COLOR = "color";
+
+            public static final String TIMESTAMP_CREATED = "timestamp_created";
+        }
+
+        public static final String CREATE_QUERY = String.format("CREATE TABLE %1$s (%2$s, %3$s, %4$s, %5$s, %6$s, %7$s, %8$s, %9$s, %10$s DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, %11$s)",
+                TABLE_NAME, Cols.NAME, Cols.MAIL, Cols.PHONE, Cols.ADDRESS, Cols.URL, Cols.NOTE,
+                Cols.CARD_STYLE, Cols.COLOR, Cols.TIMESTAMP_CREATED, Cols.UUID);
     }
 
 }
--- app/src/main/java/com/ofalvai/aircard/db/SavedCardHelper.java
+++ app/src/main/java/com/ofalvai/aircard/db/SavedCardHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.ofalvai.aircard.db.DbSchema.SavedCardsTable;
-
 public class SavedCardHelper  extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
@@ -18,11 +16,14 @@ public class SavedCardHelper  extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = String.format("CREATE TABLE %1$s (%2$s, %3$s, %4$s, %5$s, %6$s, %7$s, %8$s, %9$s, %10$s DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, %11$s)",
-                SavedCardsTable.TABLE_NAME, SavedCardsTable.Cols.NAME, SavedCardsTable.Cols.MAIL, SavedCardsTable.Cols.PHONE,
-                SavedCardsTable.Cols.ADDRESS, SavedCardsTable.Cols.URL, SavedCardsTable.Cols.NOTE,
-                SavedCardsTable.Cols.CARD_STYLE, SavedCardsTable.Cols.COLOR, SavedCardsTable.Cols.TIMESTAMP_SAVE, SavedCardsTable.Cols.UUID);
-        db.execSQL(query);
+
+        // Saved cards
+        String savedCardsQuery = DbSchema.SavedCardsTable.CREATE_QUERY;
+        db.execSQL(savedCardsQuery);
+
+        // My cards
+        String myCardsQuery = DbSchema.MyCardsTable.CREATE_QUERY;
+        db.execSQL(myCardsQuery);
     }
 
     @Override
