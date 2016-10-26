70dc8ec7bb515bbcdc08aa1a886aa7b5465e1714
Olivér Falvai <ofalvai@gmail.com>
2016-10-26 17:42:27 +0200

    My card list item layout

--- app/src/main/java/com/ofalvai/aircard/presentation/mycards/MyCardsActivity.java
+++ app/src/main/java/com/ofalvai/aircard/presentation/mycards/MyCardsActivity.java
@@ -2,11 +2,8 @@ package com.ofalvai.aircard.presentation.mycards;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.ofalvai.aircard.R;
 import com.ofalvai.aircard.model.Card;
@@ -34,14 +31,7 @@ public class MyCardsActivity extends AppCompatActivity implements MyCardsContrac
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
